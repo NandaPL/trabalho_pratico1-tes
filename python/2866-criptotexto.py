63f3ccb9a5ca211c3c04150af9edaa51f36e85b6
NandaPL <fpl.snf19@uea.edu.br>
2022-04-14 18:44:10 -0400

    refactor: alterando nome das variaveis

--- python/2866-criptotexto.py
+++ python/2866-criptotexto.py
@@ -1,23 +1,24 @@
 casos = input()
 
-if casos.isdigit():
+if casos.isdigit() and not casos == '0':
     while casos:
         casos = int(casos)
         palavra = input()
-        contrario = str()
+        texto_oculto = str()
         count = 0
 
         for letra in palavra:
             if letra.islower():
-                contrario += letra
+                texto_oculto += letra
                 count += 1
 
         if count > 0:
-            saida = contrario[::-1]
-            print(contrario[::-1])
+            print('Texto oculto: ' + texto_oculto[::-1])
         else:
             print("Mensagem desconhecida")
 
         casos -= 1
+elif casos == '0':
+    print('Sem mensagem para descobrir.')
 else:
     print("Entrada inválida!")
