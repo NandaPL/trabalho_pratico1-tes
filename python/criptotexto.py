bf85d26c8bfef9c451f55949118ad17afccda752
NandaPL <fpl.snf19@uea.edu.br>
2022-04-15 13:55:31 -0400

    fix: ajuste de metodo

--- python/criptotexto.py
+++ python/criptotexto.py
@@ -1,36 +1,23 @@
-def main(casos, palavras):
-    if casos.isdigit() and not casos == '0':
-        casos = int(casos)
+def main(palavra):
+    if not palavra.isdigit():
+        texto_oculto = str()
+        count = 0
 
-        if casos > 1:
-            for palavra in range(len(palavras)):
-                texto_oculto = str()
-                count = 0
+        for letra in palavra:
+            if letra.islower():
+                texto_oculto += letra
+                count += 1
 
-                for letra in palavras[palavra]:
-                    if letra.islower():
-                        texto_oculto += letra
-                        count += 1
-
-                if count > 0:
-                    print('Texto oculto: ' + texto_oculto[::-1])
-                else:
-                    print('Sem mensagem oculta')
+        if count > 0:
+            print('Texto oculto: ' + texto_oculto[::-1])
         else:
-            c = 0
-            texto_escondido = str()
-
-            for l in palavras:
-                if l.islower():
-                    texto_escondido += l
-                    c += 1
-
-            if c > 0:
-                print('Texto oculto: ' + texto_escondido[::-1])
-            else:
-                print('Sem mensagem oculta')
-
-    elif casos == '0':
-        print('Sem mensagem para ser descoberta.')
+            print('Sem mensagem oculta')
     else:
-        print("Entrada inválida!")
+        print('Entrada inválida!')
+
+
+casos = int(input())
+
+for i in range(0, casos):
+    palavra = input('Informe a mensagem: ')
+    main(palavra)
